6dd019fb24fcc9c343523104bcf0f1f94c56723c
phperesdasilva <phperesdasilva@gmail.com>
2021-08-26 16:22:25 -0300

    teste commit no notebook

--- Repo_ProjetoSem/Tkinter/tkinter_button_test.py
+++ Repo_ProjetoSem/Tkinter/tkinter_button_test.py
@@ -12,7 +12,7 @@ def ctd():
     numero = Label(root, text='Menos')
     numero.pack()
 
-btn1 = Button(root, text='+1', padx=50, pady=50, command=ctu)
+btn1 = Button(root, text='+1', padx=60, pady=50, command=ctu)
 btn1.pack()
 btn2 = Button(root, text='-1', padx=50, pady=50, command=ctd)
 btn2.pack()
