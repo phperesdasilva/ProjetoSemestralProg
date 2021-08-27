b32159ef7da3d5b228088bbb9493260709533872
phperesdasilva <phperesdasilva@gmail.com>
2021-08-27 19:35:08 -0300

    velocidade simultânea

--- Repo_ProjetoSem/Projeto_2sem/teste_vel_servo.py
+++ Repo_ProjetoSem/Projeto_2sem/teste_vel_servo.py
@@ -15,7 +15,7 @@ board.digital[base].mode = SERVO
 board.digital[x].mode = SERVO 
 board.digital[y].mode = SERVO 
 
-def rotateServo(pin, prev, angle):
+def rotateServo(pin1, pin2, pin3, pin4, prev, angle):
     if(angle<prev):
         t=-1
     elif(angle==prev):
@@ -28,28 +28,33 @@ def rotateServo(pin, prev, angle):
     else:
         t=1
     for i in range (prev, angle, t):   
-        board.digital[pin].write(i)
+        board.digital[pin1].write(i)
+        board.digital[pin2].write(i)
+        board.digital[pin3].write(i)
+        board.digital[pin4].write(i)
         print(i)
         sleep(0.03)
 
 #MÉTODO INEFICAZ DE SEGURANÇA
-def segurança():
-    rotateServo(x, 90, 90)
-    rotateServo(y, 50, 50)
+# def segurança():
+#     rotateServo(x, 90, 90)
+#     rotateServo(y, 50, 50)
 
-prevGarra = 90
-prevBase = 100
+# prevGarra = 90
+# prevBase = 0
 
-rotateServo(garra, 90, 90)
-rotateServo(base, 100, 100)
-segurança()
+# rotateServo(garra, 90, 90)
+# rotateServo(base, 100, 100)
+# segurança()
 
 while True:
 
-    segurança()
-    rotateServo(garra, prevGarra, 180)
-    rotateServo(base, prevBase, 180)
-    sleep(0.5)
-    rotateServo(garra, 180, prevGarra)
-    rotateServo(base, 180, prevBase)
-    sleep(0.5)
+    # segurança()
+    # rotateServo(garra, prevGarra, 180)
+    # rotateServo(base, prevBase, 180)
+    # sleep(0.5)
+    # rotateServo(garra, 180, prevGarra)
+    # rotateServo(base, 180, prevBase)
+    # sleep(0.5)
+    rotateServo(garra, base, x, y, 50, 90)
+    rotateServo(garra, base, x, y, 90, 50)
