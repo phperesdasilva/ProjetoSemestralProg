353ae2cf7bd5410a307437445a1aee519d46f9a9
phperesdasilva <phperesdasilva@gmail.com>
2021-10-07 19:08:56 -0300

    .
    .

--- Repo_ProjetoSem/Projeto_2sem/Teste_class.py
+++ Repo_ProjetoSem/Projeto_2sem/Teste_class.py
@@ -1,8 +1,9 @@
+from pyfirmata import SERVO, Arduino
+
 from robArm import robArm
 from robGUI import showScreen
-from pyfirmata import Arduino, SERVO 
 
-board = Arduino('COM1')
+board = Arduino("COM1")
 
 robo = robArm(3, 9, 10, 11, board)
 
